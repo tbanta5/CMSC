120796281a187a8b365567abb8d820ae75402935
hydruga <aandjawesomesauce@gmail.com>
2024-02-04 07:28:21 -0500

    Adding html .. not sure if we want to go this route though

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -8,6 +8,11 @@ import (
 
 func (app *application) Routes() *httprouter.Router {
 	router := httprouter.New()
+
+	router.HandlerFunc(http.MethodGet, "/", app.index)
+
 	router.HandlerFunc(http.MethodGet, "/v1/liveness", app.liveness)
+	router.HandlerFunc(http.MethodGet, "/v1/readiness", app.readiness)
+
 	return router
 }
